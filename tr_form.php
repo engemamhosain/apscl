225559b87b61fa107bb94fe6332dc919f02ee2d9
nurulamin <nurulamin@gmail.com>
2020-03-17 00:50:03 +0600

    tr form

--- tr_form.php
+++ tr_form.php
@@ -1,14 +1,4 @@
 <?php include 'includes/header-new.php';?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Colorlib Templates">
-    <meta name="author" content="Colorlib">
-    <meta name="keywords" content="Colorlib Templates">
 
     <!-- Title Page-->
     <title>TR Form</title>
@@ -25,41 +15,45 @@
 
     <!-- Main CSS-->
     <link href="tr_form/css/main.css" rel="stylesheet" media="all">
-</head>
 
 
-<body>
-    <div class="page-wrapper" style="background: #2c3e50;"> 
-        <div class="wrapper wrapper--w680">
+
+
+    
             <div class="card card-1">
-                <div class="card-heading"></div>
+                <div class="card-heading"><h2 class="title">TROUBLE REPORT</h2></div>
                 <div class="card-body">
-                    <h2 class="title">TROUBLE REPORT</h2>
+                    
                        
                             <script type="text/template" id="tmp_tr_list">
                                 
                                 <div class="row row-space">
                                    <div class="col-2"> 
-                                        <div class="input-group">
-                                            <input class="input--style-1" type="text" placeholder="<%= Tr_No.options.label %>" name="<%= Tr_No.name %>">                              
+
+                                        <div class="input-field">
+                                            <input type="text" name="<%= Tr_No.name %>">   
+                                            <label for="<%= Tr_No.name %>"><%= Tr_No.options.label %></label>                          
                                         </div>
                                     </div> 
                                      <div class="col-2"> 
-                                        <div class="input-group">                             
-                                            <input class="input--style-1" type="text" placeholder="<%= Equipment.options.label %>" name="<%= Equipment.name %>">
+                                        <div class="input-field">                             
+                                            <input type="text" name="<%= Equipment.name %>">
+                                            <label for="<%= Equipment.name %>"><%= Equipment.options.label %></label>
                                         </div>
                                     </div>
                                   </div>
 
                                    <div class="row row-space">
                                    <div class="col-2"> 
-                                        <div class="input-group">
-                                            <input class="input--style-1" type="text" placeholder="<%= Name_of_the_Reporting_Persion.options.label %>" name="<%= Name_of_the_Reporting_Persion.name %>">                              
+                                        <div class="input-field">
+                                            <input type="text" name="<%= Name_of_the_Reporting_Persion.name %>"> 
+                                            <label for="<%= Name_of_the_Reporting_Persion.name %>"><%= Name_of_the_Reporting_Persion.options.label %></label>                             
                                         </div>
                                     </div> 
                                      <div class="col-2"> 
-                                        <div class="input-group">                             
-                                            <input class="input--style-1" type="text" placeholder="<%= Location.options.label %>" name="<%= Location.name %>">
+                                        <div class="input-field">                             
+                                            <input type="text" name="<%= Location.name %>">
+                                            <label for="<%= Location.name %>"><%= Location.options.label %></label>
                                         </div>
                                     </div>
                                   </div>  
@@ -67,13 +61,14 @@
 
                                 <div class="row row-space">
                                     <div class="col-2">
-                                        <div class="input-group">
-                                            <input class="input--style-1 js-datepicker" type="Date" placeholder="<%= Date.options.label %>" name="<%= Date.options.label %>">
+                                        <div class="input-field">
+                                            <input js-datepicker" type="Date" name="<%= Date.options.label %>">
                                             <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                            <label for="<%= Date.options.label %>"><%= Date.options.label %></label>
                                         </div>
                                     </div>
                                     <div class="col-2">
-                                        <div class="input-group">
+                                        <div class="input-field">
                                             <div class="rs-select2 js-select-simple select--no-search">
                                                 <select name="<%= Designation.name %>">
                                                     <option disabled="disabled" selected="selected"><%= Designation.options.label %>:</option>
@@ -82,29 +77,30 @@
                                                     <option>Other</option>
                                                 </select>
                                                 <div class="select-dropdown"></div>
+                                                
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                
 
-                                <label for="comment"><%= Description_of_Trouble.options.label %>:</label>
-                                <div class="input-group">
-                                    <textarea class="input--style-1" style="width: 100%"  rows="5" id="comment" name="<%= Description_of_Trouble.name %>"></textarea>
+                               
+                                <div class="input-field">
+                                    <textarea rows="5" id="comment" name="<%= Description_of_Trouble.name %>"></textarea>
+                                     <label for="<%= Description_of_Trouble.name %>"><%= Description_of_Trouble.options.label %>:</label>
                                 </div>
 
-                                 <div class="input-group">
-                                    <input list="department" class="input--style-1" type="text" placeholder="<%= Referred_to.options.label %>:" name="<%= Referred_to.name %>">
-                                    
-                                      <datalist id="department">                                        
-                                      </datalist>
+                                 <div class="input-field">
+                                    <input list="department" type="text" name="<%= Referred_to.name %>">
+                                    <label for="<%= Referred_to.name %>"><%= Referred_to.options.label %>:</label>
+                                      <datalist id="department"></datalist>
                                 </div>
                                   
                                 
                                 <div class="row row-space">
                                   
                                        
-                                            <button  style="width:100%" class="btn btn--radius btn--green" type="submit" onclick="postTr()">Submit</button>
+                                            <button  class="btn waves-effect waves-light" type="submit" onclick="postTr()">Submit</button>
                                        
                                       
                                  </div> 
@@ -117,8 +113,7 @@
                  
                 </div>
             </div>
-        </div>
-    </div>
+       
 
 
     <!-- Jquery JS-->
@@ -131,15 +126,13 @@
     <!-- Main JS-->
     <script src="tr_form/js/global.js"></script>
 
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
-</html>
 
   <?php include 'includes/footer-new.php';?>
     <script src="asset/js/app/pages/tr_list.js"></script>
 
 <script type="text/javascript">
-    function postTr(){
+       function postTr(){
     console.log("tr post");
         var obj = {};
           $('.input--style-1').each(function(i, v){
