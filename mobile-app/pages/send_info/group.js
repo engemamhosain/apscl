2b5d9348330ea7b733137dabcbf3464992b9683b
emamhasan1137 <emamhasan1137@gmail.com>
2021-01-21 01:34:38 +0600

    menu

--- mobile-app/pages/send_info/group.js
+++ mobile-app/pages/send_info/group.js
@@ -6,7 +6,7 @@ get("chat_group.php",{},function (result){
       
           <li class="collection-item avatar" onclick="goDetail('${element.id}/${element.group_name}')">
           <i class="material-icons circle purple">people</i>
-          <span class="title">Plant Id ${element.group_name}</span>
+          <span class="title">${element.group_name}</span>
           <p>${element.group_detail}</p>
           <a href="#!" class="secondary-content"><i class="material-icons" style="color:purple">chevron_right</i></a>
         </li>
